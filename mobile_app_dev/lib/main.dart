b75dad19047c075533478efa320e327666394495
Vignesh SK <vigsk17@gmail.com>
2021-12-19 20:40:04 -0500

    Added Firebase

--- mobile_app_dev/lib/main.dart
+++ mobile_app_dev/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mobile_app_dev/firebase_options.dart';
+
 import 'bars.dart';
 
 // Starts the app directly w/ the MyApp class
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 // Basically the root of the app, defines basic things like themeing and routes
 
@@ -14,16 +23,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MAD App',
-      theme: FlexColorScheme.light(scheme: FlexScheme.mango).toTheme,
-      darkTheme: FlexColorScheme.dark(scheme: FlexScheme.mango).toTheme,
-      locale: const Locale('us'),
-      initialRoute: '/BarsScreen',
-      routes: {
-        '/BarsScreen': (context) => BarsScreen()
-      }
-    );
+        title: 'MAD App',
+        theme: FlexColorScheme.light(scheme: FlexScheme.mango).toTheme,
+        darkTheme: FlexColorScheme.dark(scheme: FlexScheme.mango).toTheme,
+        locale: const Locale('us'),
+        initialRoute: '/BarsScreen',
+        routes: {'/BarsScreen': (context) => BarsScreen()});
   }
-
 }
-
